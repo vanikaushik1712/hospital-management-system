e07607abaa650c2183e71d04781f802253b87bc9
vanikaushik1712 <vanikaushik1712@gmail.com>
2026-05-27 12:45:49 +0530

    Add production API URL

--- hospital-frontend/src/api.js
+++ hospital-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api'
 });
 
 export const getPatients = () => API.get('/patients');
